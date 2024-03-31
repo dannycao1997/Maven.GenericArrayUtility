d9c86ee098cbdd556c2a86b17d7e2a33ebda224b
Danny Cao <dannycao1997@yahoo.com>
2024-03-31 00:31:47 -0400

    learning

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,25 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
-}
+public class ArrayUtility<T> { // generic array setup
+    T[] inputArray; // inputArray with Type T elements
+    public ArrayUtility(T[] inputArray) { // constructor
+        this.inputArray = inputArray;
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        ArrayUtility<T> mergedUtility = new ArrayUtility<>(mergeArrays(arrayToMerge));
+        return;
+    }
+
+
+
+
